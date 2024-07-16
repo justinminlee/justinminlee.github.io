91d5d09699f5859b53d78a51253eeed0b6256561
Justin Lee <smsc19970205@gmail.com>
2024-07-16 18:23:56 +1000

    initial commit

--- git_portfolio/src/App.js
+++ git_portfolio/src/App.js
@@ -2,24 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sections = [
+    {
+      "Section 1": "Computer Vision",
+      "Description": "A brief overview of what you can expect from this section.",
+      "Parts" : [
+        { title: 'Introduction to Our Blog', "Description": "A brief overview of what you can expect from this part.", "Date_Published": "2021-01-01" , "Author": "John Doe" , "Date Modified": "2021-01-02" },
+        { title: 'Chapter 1: The Beginning', "Description": "The first chapter in our blog series.", "Date_Published": "2021-01-03" , "Author": "Jane Doe" , "Date Modified": "2021-01-04" },
+        { title: 'Chapter 2: The Journey Continues', "Description": "The second chapter in our blog series.", "Date_Published": "2021-01-05" , "Author": "John Doe" , "Date Modified": "2021-01-06" }
+      ]
+    },
+
+    {
+      "Section 2": "The Middle of Our Blog",
+      "Description": "A brief overview of what you can expect from this section.",
+      "Parts" : [
+        { title: 'Chapter 3: The Plot Thickens', "Description": "The third chapter in our blog series.", "Date_Published": "2021-01-07" , "Author": "Jane Doe" , "Date Modified": "2021-01-08" },
+        { title: 'Chapter 4: The Climax', "Description": "The fourth chapter in our blog series.", "Date_Published": "2021-01-09" , "Author": "John Doe" , "Date Modified": "2021-01-10" }
+      ]
+    },
+
+    {
+      "Section 3": "The End of Our Blog",
+      "Description": "A brief overview of what you can expect from this section.",
+      "Parts" : [
+        { title: 'Chapter 5: The End', "Description": "The final chapter in our blog series.", "Date_Published": "2021-01-11" , "Author": "Jane Doe" , "Date Modified": "2021-01-12" }
+      ]
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableOfContents sections={sections} />
     </div>
   );
+
 }
+
 
 export default App;
