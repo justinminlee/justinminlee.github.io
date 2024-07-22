b3b23fc9edffdd3cae322571eee1ad5dde973ffb
Justin Lee <smsc19970205@gmail.com>
2024-07-22 19:57:42 +1000

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">John Doe</div>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header; 
