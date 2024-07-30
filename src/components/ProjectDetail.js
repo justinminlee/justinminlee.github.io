b823a8b97f163a16bf71b38781831d5bf6db48b3
Justin Lee <smsc19970205@gmail.com>
2024-07-30 14:05:12 +1000

    add flashcard component

--- src/components/ProjectDetail.js
+++
@@ -1,2 +0,0 @@
-// This for routing the project cards to the project detail page
-// Need to create a new component for the project detail page
---
+++ src/components/flashcardpj.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function flashcardpj() {
+  return (
+    <section id="projectdetail" className="projectdetail">
+      <h2>About Me</h2>
+      <p>
+        I'm a final year data science student at QUT who loves coding
+      </p>
+      <p>
+        Currently I am looking for a junior data scientist/analyst or software dev/eng position job.
+      </p>
+    </section>
+  );
+}
+
+export default flashcardpj;
