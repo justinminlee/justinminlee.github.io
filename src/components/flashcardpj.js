0b3ef9550d04944f4c0e4b5b289de5d467b7dc98
Justin Lee <smsc19970205@gmail.com>
2024-08-01 15:52:46 +1000

    update card

--- src/components/flashcardpj.js
+++ src/components/flashcardpj.js
@@ -5,7 +5,7 @@ function flashcardpj() {
     <section id="flashcardpj" className="flashcardpj">
       <h2>About Me</h2>
       <p>
-        I'm a final year data science student at QUT who loves coding
+        I'm a final year data science student at QUT who loves coding!
       </p>
       <p>
         Currently I am looking for a junior data scientist/analyst or software dev/eng position job.
